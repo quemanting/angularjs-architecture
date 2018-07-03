c04e4974ad94ae082ef4814abfdea25476e827dc
quemanting <694682740@qq.com>
2018-07-03 14:51:32 +0800

    提交一波66

--- view/app/test/test.js
+++ view/app/test/test.js
@@ -1,6 +1,19 @@
-define(['app/config/app'], function (app) {
+define(['app/config/app','layui'], function (app) {
     app.registerController('testCtrl',function ($scope) {
+        /**
+         * 请求业务
+         */
+        var requestObj = $scope.requestObj = {
+            init:function(){
+                requestObj.markdownInit();
+            },
+            markdownInit:function(){
+                layui.use('code', function(){ //加载code模块
+                    layui.code(); //引用code方法
+                });
+            },
+        };
 
-
+        requestObj.init();
     })
 });
