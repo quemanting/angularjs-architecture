359d5d35f8083e3e13b628352dd102c82cdc8889
quemanting <694682740@qq.com>
2018-07-04 16:54:15 +0800

    提交一波669

--- view/app/document/front_end_specification/front_end_specification.ctrl.js
+++ view/app/document/front_end_specification/front_end_specification.ctrl.js
@@ -20,7 +20,7 @@ define(['app/config/app','showdown','mCustomScrollbar'], function (app,showdown)
                 /*layui.use('code', function(){ //加载code模块
                     layui.code(); //引用code方法
                 });*/
-                $http.get('app/document/markdown/one.md').success(function (data) {
+                $http.get('app/document/markdown/one.md').success(function(data) {
                     var converter = new showdown.Converter();
                     document.getElementById("result").innerHTML = converter.makeHtml(data);
                 });
